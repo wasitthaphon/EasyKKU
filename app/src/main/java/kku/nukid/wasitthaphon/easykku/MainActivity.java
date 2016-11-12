6d9cffa6da05c8d8dd0a7c36cd1f08e1dc296260
beer <wasitthaphon@kkumail.com>
2016-11-12 10:24:40 +0700

    Start App

---
+++ app/src/main/java/kku/nukid/wasitthaphon/easykku/MainActivity.java
@@ -0,0 +1,13 @@
+package kku.nukid.wasitthaphon.easykku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
