6cfe4f52e798e07f806b0e20e6a9a6daa557edb0
beer <wasitthaphon@kkumail.com>
2016-11-12 14:12:07 +0700

    Have Space Completed

--- app/src/main/java/kku/nukid/wasitthaphon/easykku/SignUpActivity.java
+++ app/src/main/java/kku/nukid/wasitthaphon/easykku/SignUpActivity.java
@@ -2,12 +2,58 @@ package kku.nukid.wasitthaphon.easykku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit (ประกาศตัวแปร)
+    private EditText nameEditText, phoneEditText,
+            userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;      // Ctrl + Space autoFill
+    private String nameString, phoneString, userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);  // Alt + Enter --> cast to
+        phoneEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+
+        //SignUp Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Get Value From Edit Text
+                nameString = nameEditText.getText().toString().trim();
+                phoneString = phoneEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || phoneString.equals("") ||
+                        userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+                    Log.d("12novV1", "Have Space");
+
+                }
+
+
+            }   //on Click
+        });
+
+    }   // Main Method
+
+}   // Main Class
