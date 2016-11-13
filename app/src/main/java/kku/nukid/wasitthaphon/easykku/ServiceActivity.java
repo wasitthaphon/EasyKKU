0354dc590af851b3139b38f736f41c209b02e82a
beer <wasitthaphon@kkumail.com>
2016-11-13 14:57:11 +0700

    Call Completed

--- app/src/main/java/kku/nukid/wasitthaphon/easykku/ServiceActivity.java
+++ app/src/main/java/kku/nukid/wasitthaphon/easykku/ServiceActivity.java
@@ -1,5 +1,12 @@
 package kku.nukid.wasitthaphon.easykku;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.ExpandedMenuView;
@@ -43,8 +50,40 @@ public class ServiceActivity extends AppCompatActivity {
 
     }   //Main Method
 
-    private void ConfirmCall(String nameString, String phoneString) {
+    private void ConfirmCall(String nameString, final String phoneString) {
 
-    }
+        AlertDialog.Builder builder = new AlertDialog.Builder(ServiceActivity.this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("Please Confirm");
+        builder.setMessage("คุณต้องการโทรหา " + nameString + " ?");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:=" + phoneString));
+                if (ActivityCompat.checkSelfPermission(ServiceActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+
+            }
+        });
+        builder.show();
+    }   // confirm
 
 }   //Main Class
