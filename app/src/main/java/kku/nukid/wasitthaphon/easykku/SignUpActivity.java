fe525eb12cda4b4f7a181069437d72bf5f86ecc0
beer <wasitthaphon@kkumail.com>
2016-11-13 10:57:50 +0700

    SentDataToSerVer V1

--- app/src/main/java/kku/nukid/wasitthaphon/easykku/SignUpActivity.java
+++ app/src/main/java/kku/nukid/wasitthaphon/easykku/SignUpActivity.java
@@ -1,10 +1,14 @@
 package kku.nukid.wasitthaphon.easykku;
 
+import android.app.AlertDialog;
+import android.app.VoiceInteractor;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +18,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 import org.jibble.simpleftp.SimpleFTP;
 
 import java.io.File;
+
+import static android.net.wifi.WifiConfiguration.Status.strings;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -30,6 +43,12 @@ public class SignUpActivity extends AppCompatActivity {
     imagePathString, imageNameString;
     private Uri uri;
     private boolean aBoolean = true;
+    private String urlAddUser = "http://swiftcodingthai.com/kku/add_user_master.php";
+    private String urlImage = "http://swiftcodingthai.com/kku/Image";
+    /*
+        then FileZilla --> SomeFiles --> CopyUrl --> Phase in Browser --> Clear protocol then add
+        (swiftcodingthai.com/kku/Image/IMG_xxxxx.jpg)
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +91,7 @@ public class SignUpActivity extends AppCompatActivity {
                 } else {
                     //Choose Image OK
                     upLoadImageToServer();
+                    upLoadStringToServer(); //If creat with Anonymous it means in main Method outer cant use it
 
                 }
 
@@ -89,6 +109,62 @@ public class SignUpActivity extends AppCompatActivity {
             }// onClick
         });
     }   // Main Method
+
+    private void upLoadStringToServer() {
+
+        AddNewUser addNewUser = new AddNewUser(SignUpActivity.this);
+        addNewUser.execute(urlAddUser);
+
+    }   //upLoad
+
+    //Create Inner Class
+    private class AddNewUser extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+
+        public AddNewUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("Name", nameString)
+                        .add("Phone", phoneString)
+                        .add("User", userString)
+                        .add("Password", passwordString)
+                        .add("Image", urlImage + imageNameString)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("13novV1", "e doIn ==> " + e.toString());
+                return null;
+            }
+        }   // doInBack --> Try to connect Network Sent Result to onPost
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("13novV1", "Result ==> " + s);
+            if (Boolean.parseBoolean(s)) {
+                Toast.makeText(context, "Upload Success", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                Toast.makeText(context, "Upload Failed", Toast.LENGTH_SHORT).show();
+            }
+        }   // onPost
+
+    }//<Sent ,  , Result>   AddNewUser Class
 
     private void upLoadImageToServer() {
         //Change Policy
